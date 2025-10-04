fc08f4233601edc4ea3f27247cdb4a6fd7e12fa3
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-10-04 16:54:39 +0200

    Initial commit

---
+++ src/main/java/com/github/jbence1994/calendarium/CalendariumApplication.java
@@ -0,0 +1,13 @@
+package com.github.jbence1994.calendarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendariumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendariumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jbence1994/calendarium/CalendariumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jbence1994.calendarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendariumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
