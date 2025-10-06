6dfc297e3f322979c0f6521ba367c21ff7812105
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2025-10-06 12:27:45 +0200

    Implement data access layer using Flyway Maven plugin (#3)

---
+++ src/main/resources/db/migration/V1__database_schema.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS calendarium
+    DEFAULT CHARACTER SET utf8
+    COLLATE utf8_hungarian_ci;
+
+USE calendarium;
---
+++ src/test/java/com/github/jbence1994/calendarium/ApplicationTests.java
@@ -0,0 +1,10 @@
+package com.github.jbence1994.calendarium;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
