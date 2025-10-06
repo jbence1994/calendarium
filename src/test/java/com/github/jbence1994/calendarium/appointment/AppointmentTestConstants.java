de6aab6949457910d182f8c46c827dda94304c42
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2025-10-06 17:24:49 +0200

    Add `description` field to `Appointment` and `AppointmentDto` (#6)

--- src/test/java/com/github/jbence1994/calendarium/appointment/AppointmentTestConstants.java
+++ src/test/java/com/github/jbence1994/calendarium/appointment/AppointmentTestConstants.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 public interface AppointmentTestConstants {
     UUID APPOINTMENT_ID = UUID.fromString("6198ccd7-c323-4734-b134-051b75120e7c");
     String APPOINTMENT_NAME = "Appointment#1";
+    String APPOINTMENT_DESCRIPTION = "Appointment description.";
     LocalDateTime APPOINTMENT_START_DATE = LocalDateTime.of(2025, 10, 6, 10, 0, 0);
     LocalDateTime APPOINTMENT_END_DATE = LocalDateTime.of(2025, 10, 6, 11, 0, 0);
 }
